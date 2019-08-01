7944c933d7d0b6fc54ac99d79e99f404383c6b3a
grimgoon <alexander@hermansson.us>
2019-08-01 09:30:36 +0200

    Lecture 14

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,55 +3,88 @@ import { GraphQLServer } from "graphql-yoga";
 
 //Scalar types - String, Boolean, Int, Float, ID
 
+// id: ID!
+// name: String!
+// age: Int!
+// employed: Boolean!
+// gpa: Float
+// title: String!
+// price: Float!
+// releaseYear: Int
+// rating: Float
+// inStock: Boolean
+
+// id() {
+//   return "ABC12345";
+// },
+// name() {
+//   return "Alexander";
+// },
+// age() {
+//   return 24;
+// },
+// employed() {
+//   return true;
+// },
+// gpa() {
+//   return 3.17;
+// },
+// title() {
+//   return "Memes strikes Back Volume 2";
+// },
+// price() {
+//   return 3.5;
+// },
+// releaseYear() {
+//   return 2019;
+// },
+// rating() {
+//   return 4.6;
+// },
+// inStock() {
+//   return true;
+// }
+
 // Type definitions (schema)
 const typeDefs = `
-type Query {
+  type Query {
+    me: User!
+    post: Post!
+  }
+
+  type User {
     id: ID!
     name: String!
-    age: Int!
-    employed: Boolean!
-    gpa: Float
-    title: String!
-    price: Float!
-    releaseYear: Int
-    rating: Float
-    inStock: Boolean
+    email: String!
+    age: Int
+  }
 
-}
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
+
 `;
 
 // Resolvers
 const resolvers = {
   Query: {
-    id() {
-      return "ABC12345";
+    me() {
+      return {
+        id: "12134",
+        name: "Mike",
+        email: "mike@example.com"
+      };
     },
-    name() {
-      return "Alexander";
-    },
-    age() {
-      return 24
-    },
-    employed() {
-      return true
-    },
-    gpa() {
-      return 3.17
-    },
-    title() {
-      return "Memes strikes Back Volume 2"
-    },
-    price() {
-      return 3.50
-    },
-    releaseYear() {
-      return 2019
-    },
-    rating() {
-      return 4.6
-    },
-    inStock() {
-      return true
+    post() {
+      return {
+        id: "12134",
+        title: "Mike",
+        body: "mike@example.com",
+        published: false
+      };
     }
   }
 };
