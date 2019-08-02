5d14bef7cd62c61eceff91a4cdd3e5c15921236f
grimgoon <alexander@hermansson.us>
2019-08-02 13:18:46 +0200

    Lecture 27

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -88,9 +88,28 @@ const typeDefs = `
   }
 
   type Mutation {
-    createUser(name: String!, email: String!, age: Int): User!
-    createPost(title: String!, body: String!, published: Boolean!, author: ID): Post!
-    createComment(text: String!, author: ID, post: ID): Comment!
+    createUser(data: CreateUserInput): User!
+    createPost(data: CreatePostInput): Post!
+    createComment(CreateCommentInput): Comment!
+  }
+
+  input CreateUserInput {
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  input CreatePostInput {
+    title: String!
+    body: String!
+    published: Boolean!
+    author: ID!
+  }
+
+  input CreateCommentInput {
+    text: String!
+    author: ID!
+    post: ID!
   }
 
   type User {
@@ -164,7 +183,7 @@ const resolvers = {
   },
   Mutation: {
     createUser(parent, args, ctx, info) {
-      const emailTaken = users.some(user => user.email === args.email);
+      const emailTaken = users.some(user => user.email === args.data.email);
       
       if(emailTaken) {
         throw new Error("Email already taken");
@@ -172,13 +191,13 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        ...args
+        ...args.data
       }
 
       users.push(user);
       return user;
     }, createPost(parent, args, ctx, info) {
-      const userExists = users.some(user => user.id === args.author);
+      const userExists = users.some(user => user.id === args.data.author);
 
       if(!userExists) {
         throw new Error("User not found");
@@ -186,20 +205,20 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        ...args
+        ...args.data
       }
 
       posts.push(post);
       return post;
     },
     createComment(parent, args, ctx, info) {
-      const userExists = users.some(user => user.id === args.author);
+      const userExists = users.some(user => user.id === args.data.author);
 
       if(!userExists) {
         throw new Error("User not found");
       }
       
-      const postExists = posts.some(post => post.id === args.post);
+      const postExists = posts.some(post => post.id === args.data.post);
 
       if(!postExists) {
         throw new Error("Post not found");
@@ -207,7 +226,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        ...args
+        ...args.data
       }
 
       
