ec7b6d37bd68eba01fde41efa9f3e1473aa03cc9
grimgoon <alexander@hermansson.us>
2019-08-09 09:15:48 +0200

    Lecture 35

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,9 +1,10 @@
 // Named export
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer, PubSub } from "graphql-yoga";
 
 import db from "./db";
 import Query from "./resolvers/Query";
 import Mutation from "./resolvers/Mutation";
+import Subscription from "./resolvers/Subscription";
 import User from "./resolvers/User";
 import Post from "./resolvers/Post";
 import Comment from "./resolvers/Comment";
@@ -11,16 +12,20 @@ import Comment from "./resolvers/Comment";
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Post,
   Comment
 };
 
+const pubSub = new PubSub();
+
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
   context: {
-    db
+    db,
+    pubSub
   }
 });
 
