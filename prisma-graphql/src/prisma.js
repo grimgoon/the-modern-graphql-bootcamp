7a61b3c31707da3288e1b8898cd581942cb12872
Alexander Hermansson <alexander@hermansson.us>
2019-10-20 11:17:26 +0200

    Lecture 52

--- prisma-graphql/src/prisma.js
+++ prisma-graphql/src/prisma.js
@@ -5,10 +5,50 @@ const prisma = new Prisma({
   endpoint: "http://localhost:4466"
 });
 
-prisma.query.users(null, "{id name posts { id title } }").then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.query.comments(null, "{id text author { id name } }").then(data => {
+//   console.log(JSON.stringify(data, undefined, 2));
+// });
 
-prisma.query.comments(null, "{id text author { id name } }").then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.mutation
+//   .createPost(
+//     {
+//       data: {
+//         title: "Mepe",
+//         body: "Meeepers is peepers",
+//         published: true,
+//         author: {
+//           connect: {
+//             id: 3
+//           }
+//         }
+//       }
+//     },
+//     "{ id title body published }"
+//   )
+//   .then(data => {
+//     console.log(JSON.stringify(data));
+//     prisma.query.users(null, "{id name posts { id title } }").then(data => {
+//       console.log(JSON.stringify(data, undefined, 2));
+//     });
+//   });
+
+prisma.mutation
+  .updatePost(
+    {
+      data: {
+        title: "Pleeb",
+        body: "Meeepers is peepers",
+        published: false
+      },
+      where: {
+        id: "ck1yrxel5024p0750a7f25ib2"
+      }
+    },
+    "{ id title body published }"
+  )
+  .then(data => {
+    console.log(JSON.stringify(data));
+    prisma.query.users(null, "{id name posts { id title } }").then(data => {
+      console.log(JSON.stringify(data, undefined, 2));
+    });
+  });
